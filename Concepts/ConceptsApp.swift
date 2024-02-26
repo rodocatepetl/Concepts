5e3c31111fb4938bb53eb1739e8f6ac2c175ad66
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-02-26 16:48:15 -0600

    Initial Commit

---
+++ Concepts/ConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConceptsApp.swift
+//  Concepts
+//
+//  Created by Rodolfo Castillo on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
