b1fd83c91aabea2831ef8a49e9cb6435ee9f4bbe
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-03-18 14:56:18 -0600

    fix ui stuff

--- Concepts/DetailView/PhotoCompDetailView.swift
+++ Concepts/DetailView/PhotoCompDetailView.swift
@@ -4,47 +4,54 @@ struct PhotoCompDetailView: View {
     var photoComp: PhotoComp
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
-                Text(photoComp.nombre)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top)
+        GeometryReader { reader in
+            let overlayWidth = reader.size.width - 30
+            let overlayHeight = overlayWidth * (2/3)
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(photoComp.nombre)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom)
 
-                // ScrollView horizontal para los overlays
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(photoComp.overlay, id: \.self) { overlay in
-                            Image(overlay)
-                                .resizable()
-                                .scaledToFit()
-                                .padding(.bottom)
-                                .background(Color.gray.opacity(0.3))
-                                .frame(height: 254.0)
-                                .cornerRadius(10)
-                                
+                    // ScrollView horizontal para los overlays
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(photoComp.overlay, id: \.self) { overlay in
+                                Image(overlay)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(.bottom)
+                                    .background(Color.gray.opacity(0.3))
+                                    // Aquí defines el tamaño de cada imagen directamente
+                                    .frame(height: overlayHeight - 10)
+                                    .cornerRadius(10)
+                                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                            }
                         }
+                        .padding(.horizontal)
                     }
-                    .padding(.horizontal)
+
+                    .padding(/*@START_MENU_TOKEN@*/.leading, -15.0/*@END_MENU_TOKEN@*/)
+                    .frame(height: photoComp.overlay.count > 1 ? 200 : nil)
+                    
+                    // Descripción con palabras clave resaltadas
+                    Text(photoComp.detalle)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .padding(.top)
+                    
+                    // Teoría
+                    Text(photoComp.teoria)
+                    
+                    // Historia
+                    Text(photoComp.historia)
                 }
-                .padding(/*@START_MENU_TOKEN@*/.leading, -15.0/*@END_MENU_TOKEN@*/)
-                .frame(height: photoComp.overlay.count > 1 ? 200 : nil)
-                
-                // Descripción con palabras clave resaltadas
-                Text(photoComp.detalle)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                    .frame(width: /*@START_MENU_TOKEN@*/360.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
-                    .background(Color.yellow)
-                    .cornerRadius(10)
-                
-                // Teoría
-                Text(photoComp.teoria)
-                
-                // Historia
-                Text(photoComp.historia)
+                .padding()
             }
-            .padding()
         }
     }
 }
